e07dfddd5dd8c7ee84d24019d45a89da2be1a6e7
travistrotto <54992930+travistrotto@users.noreply.github.com>
2021-04-22 22:52:42 -0400

    Update animateText.js

--- animateText.js
+++ animateText.js
@@ -1,21 +1,5 @@
 function myFunction(){
 
-/*
-$("#example").typer({
-	strings: [
-	"I'm a Model",
-	"I'm a Designer",
-	"I'm a Photographer"
-	],
-	typeSpeed: 200,
-	backspaceSpeed: 20,
-	backspaceDelay: 800,
-	repeatDelay: 1000,
-	repeat: true,
-	autoStart: true,
-	startDelay: 100,
-	});
-*/
 var app = document.getElementById('app');
 
 var typewriter = new Typewriter(app, {
